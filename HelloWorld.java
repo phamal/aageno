78a4068e27e12613c64ad63dd4e1d7b65d4abbd4
phamal <phamal@bidsync.com>
2016-08-01 20:24:37 -0600

    Aug 2 2016-- revamping

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World Prakash Hamal");
+        System.out.println("Hello, World Prakash Hamal-- This is going to be really awesome.");
     }
 
 }
