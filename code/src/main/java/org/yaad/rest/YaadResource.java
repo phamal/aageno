927fbe083b7a91d24acffbd3c74d03849e82db55
phamal <phamal@bidsync.com>
2016-08-14 16:37:41 -0600

    aageno with intellij

---
+++ code/src/main/java/org/yaad/rest/YaadResource.java
@@ -0,0 +1,24 @@
+package org.yaad.rest;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/api")
+public class YaadResource {
+
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+}
