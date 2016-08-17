b6a59c68c6192dcd2c40a5764fec852d0251a868
phamal <phamal@bidsync.com>
2016-08-17 08:13:42 -0600

    New API

---
+++ code/src/main/java/org/yaad/dtos/HashTag.java
@@ -0,0 +1,19 @@
+package org.yaad.dtos;
+
+/**
+ * Created by phamal on 8/16/16.
+ */
+public class HashTag
+{
+	private String hashtag;
+
+	public String getHashtag()
+	{
+		return hashtag;
+	}
+
+	public void setHashtag(String hashtag)
+	{
+		this.hashtag = hashtag;
+	}
+}
