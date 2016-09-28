2c2d8fca2a3ca4f9e81dc4d665f0524006fc5e6d
phamal <phamal@bidsync.com>
2016-09-28 09:16:29 -0600

    Automating mundane work

--- scripts/phitools.sh
+++ scripts/phitools.sh
@@ -1,51 +1,118 @@
 #!/bin/sh
 
+WILDFLY_BASE="/apps/web"
+LOG_BASE="/apps/web/logs"
+ADMIN_PORT=""
+DEBUG_PORT=""
+CODE_PATH=""
+ARTIFACT_PATH=""
+ARTIFACT_FILE=""
+DOMAIN=$1;
+ACTION=$2;
 
 
-function printHelpAndExit {
-  echo "Usage: $0 {bidreleased|todaystable|killtheport|auctionss} "
-	exit 1;
+function dist {
+    cd $CODE_PATH;
+    ant dist
 }
 
-function bidreleased {
-    curl "http://dev.bidsync.com:9480/notification-web/rs/event/new/BidReleased/DPXWEBBID/"$1
+function start {
+    echo "Starting "$DOMAIN;
 }
 
-function auctionss {
-    curl "http://dev.bidsync.com:9480/notification-web/rs/event/new/LinksBidReleased_For_SuggestedSuppliers/DPXAUCTION/"$1
+function undeploy {
+    echo "Undeploying "$DOMAIN ;
 }
 
-function todaystable {
-    curl "http://dev.bidsync.com:9480/notification-web/rs/test/notificationMessagesTableForToday"
+function stop {
+    echo "Stopping "$DOMAIN;
 }
 
-function killtheport {
-   # kill -15 $(`lsof -i :$1 -t` )
-   pid=$(lsof -i:$1 -t); kill -TERM $pid || kill -KILL $pid
+function restart {
+    echo "Restarting "$DOMAIN ;
 }
 
+function redeploy {
+    echo "Redeploying "$DOMAIN;
+}
+
+function main {
 
 
-case $1 in
-    bidreleased)
-      echo  "Bid was released " $2
-      bidreleased $2
-      echo ""
-      ;;
-    todaystable)
-      todaystable
-      echo ""
-      ;;
-    killtheport)
-      killtheport $1
-    ;;
-    auctionss)
-      auctionss $2
-    ;;
+    case $DOMAIN in
+    dpx)
+        ADMIN_PORT="9191"
+        DEBUG_PORT="9109"
+        CODE_PATH="/apps/code/bidsync/dpx"
+        ARTIFACT_PATH="/apps/code/bidsync/dpx/target/deploy"
+        ARTIFACT_FILE="dpx-war.war"
+        ;;
+    cas)
+        ADMIN_PORT="9291"
+        DEBUG_PORT="9209"
+        CODE_PATH="/apps/code/bidsync/cas"
+        ARTIFACT_FILE="bidsync-cas-war.war"
+        ;;
+    bidsync)
+        ADMIN_PORT="9391"
+        DEBUG_PORT="9309"
+        CODE_PATH="/apps/code/bidsync"
+        ARTIFACT_PATH="/apps/code/bidsync/bidsync-app/target/deploy"
+        ARTIFACT_FILE="bidsync-app-ear.ear"
+        LOGFILE="$LOG_BASE/wf_refactor.log"
+        ;;
+    notification)
+        ADMIN_PORT="9491"
+        DEBUG_PORT="9409"
+        CODE_PATH="/apps/code/bidsync/notification"
+        ARTIFACT_PATH="/apps/code/bidsync/notification/target/deploy"
+        ARTIFACT_FILE="notification-ear.ear"
+        ;;
+    idp)
+        echo "idp is not implemented on wildfly9 yet"
+        ADMIN_PORT="9691"
+        DEBUG_PORT="9609"
+        ;;
+    picketlink)
+        echo "picketlink is not implemented on wildfly9 yet"
+        ADMIN_PORT="9791"
+        DEBUG_PORT="9709"
+        ;;
+    security)
+        SECURITY="true"
+      	;;
     *)
-      echo "Unknow  command: $1"
-      printHelpAndExit
-      ;;
-esac
+      	echo "Wildfly application \"$1\" not found!"
+        printHelpAndExit
+      	;;
+  esac
+
+  case $ACTION in
+    dist)
+        dist
+        ;;
+    start)
+        start
+        ;;
+    restart)
+        restart
+        ;;
+    stop)
+        stop
+        ;;
+    undeploy)
+        undeploy
+        ;;
+    redeploy)
+        redeploy
+        ;;
+    *)
+        echo "Action not found "
+        ;;
+    esac
+}
 
 
+
+
+main
--- scripts/test.py
+++ scripts/test.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
-
-
 import json
 import requests
 import csv
 import sys, getopt
+import subprocess
+import glob
+import os
 
 DPX_TEST_URL = ""
 NOTIFICATION_TEST_URL = ""
-domain = ""
-status = ""
 
 
 # Status URLS ###
@@ -42,10 +41,8 @@ def monitorApis():
 
 
 def monitorNotification(domain):
-    response = None
     try:
         NOTIFICATION = "http://" + domain + ".bidsync.com:9480/notification-web/rs/test/notificationMessagesTableForToday"
-        print NOTIFICATION
         response = getUrlResponse(NOTIFICATION)
 
         statuscode = response.status_code;
@@ -60,12 +57,9 @@ def monitorNotification(domain):
 
 
 def monitorDPX(domain):
-    response = None
     try:
         DPX = "http://" + domain + ".bidsync.com:9180/dpx/rs/general/test"
-        print DPX
         response = getUrlResponse(DPX)
-
         statuscode = response.status_code;
         if statuscode == 200:
             printSuccess("DPX app is running")
@@ -83,15 +77,15 @@ def runNotificationTestSuite():
 def monitorJMS(domain):
     try:
         JMS = "http://" + domain + ".bidsync.com:9480/notification-web/rs/test/testjms"
+        print "Notification JMS:"
         response = getUrlResponse(JMS)
-
         if response.text == "success":
-            printSuccess("JMS in notification is running")
+            printSuccess("\tJMS in notification is running")
         else:
-            printError("Error : jms in notification is not running")
+            printError("\tError : jms in notification is not running")
 
     except IOError:
-        printError("Error connecting to notification app. Need to start the app -- wft notification start")
+        printError("\tError connecting to notification app. Need to start the app -- wft notification start")
 
 
 def monitorRef(domain):
@@ -110,12 +104,28 @@ def monitorRef(domain):
         printError("Error connecting to refactor. Need to start the app -- wft refactor start")
 
 
-def monitorRefactorBatches(domain):
-    print "Check statuses of the refactor batches"
+def monitorBatches(domain):
+    try:
+        BATCH_STATUS_API = "http://" + domain + ".bidsync.com:9380/bidsync-business-provider/rs/test/batchstatus"
+        print "Bach statuses : "
+        response = getUrlResponse(BATCH_STATUS_API)
+        json_obj = json.loads(response.text)
+        for batch in json_obj:
+            batchStatus = "\t" + batch['batchType']
+            if 'lastRunDate' in batch:
+                batchStatus += " | " + str(batch['lastRunDate'])
 
+            if batch['error']:
+                batchStatus += " | ERROR "
+            else:
+                batchStatus += " | SUCCESS "
 
-def monitorDpxBathes(domain):
-    print "Check statuses of the dpx batches"
+            if batch['error']:
+                printError(batchStatus)
+            else:
+                printSuccess(batchStatus)
+    except IOError:
+        printError("Error connecting to refactor Need to start the app -- wft notification start")
 
 
 def printError(msg):
@@ -126,10 +136,6 @@ def printSuccess(msg):
     print bcolors.OKGREEN + msg + bcolors.ENDC
 
 
-def monitorCas(domain):
-    print "Monitoring cas "
-
-
 def monitorElasticSearch(domain):
     try:
         ELASTICSEARCH = "http://" + domain + ".bidsync.com:9380/bidsync-business-provider/rs/test/elasticsearch"
@@ -138,19 +144,19 @@ def monitorElasticSearch(domain):
         json_obj = json.loads(response.text)
         status_code = json_obj["status"]
         if status_code == 200:
-            printSuccess("Elasticsearch in notification is running")
+            printSuccess("Elasticsearch is running")
         else:
             printError("Error : Elasticsearch  is not running")
 
     except IOError:
-        printError("Error connecting to refactor Need to start the app -- wft notification start")
+        printError("Error connecting to refactor Need to start the app -- wft bidsync start")
 
 
 def monitorSmartProcure(domain):
     try:
         SMARTPROCURE = "http://" + domain + ".bidsync.com:9380/bidsync-business-provider/rs/test/smartprocureurl"
         response = getUrlResponse(SMARTPROCURE)
-        if(response.text.find('app.smartprocure.us') != -1):
+        if (response.text.find('app.smartprocure.us') != -1):
             printSuccess("Smartprocure is running")
 
 
@@ -170,51 +176,132 @@ def monitorTax(domain):
 
 
 def monitorMuhimbi(domain):
-    print "Muhimbi status --- "
-
+    try:
+        DPX = "http://" + domain + ".bidsync.com:9180/dpx/rs/fileconversion/fileswaitingconversion"
+        response = getUrlResponse(DPX)
+        parsed = json.loads(response.text)
+        print json.dumps(parsed, indent=4, sort_keys=True)
+    except IOError:
+        printError("Error connecting to dpx app. Need to start the app -- wft dpx start")
 
 def monitorBlueSnap(domain):
-    print "Muhimbi bluesnap --- "
+    print "Monitor bluesnap --- "
 
 
 def monitorClassifier(domain):
-    print "Monitor extractor classifier"
+    print "Monitor extractor classifier ---"
+
 
 def monitorExtractor(domain):
-    print "Monitor extractor"
+    print "Monitor extractor ---"
+
 
 def monitorMarketo(domain):
-    print "Monitor marketo"
+    print "Monitor marketo ---"
+
+
+def monitorCas(domain):
+    print "Monitoring cas --"
+
+
+def monitorMailgun(domain):
+    print "Monitor mailgun ---"
+
+
+def monitorNovatus(domain):
+    print "Monitor novatus integration ---"
+
 
 def monitorAll(domain):
+    print "******CHECKING VARIOUS STATUSES*******"
     monitorDPX(domain)
     monitorRef(domain)
     monitorJMS(domain)
     monitorNotification(domain)
     monitorElasticSearch(domain)
     monitorSmartProcure(domain)
-    monitorDpxBathes(domain)
-    monitorRefactorBatches(domain)
+    monitorBatches(domain)
     monitorCas(domain)
     monitorMuhimbi(domain)
     monitorBlueSnap(domain)
     monitorTax(domain)
     monitorExtractor(domain)
     monitorMarketo(domain)
+    monitorClassifier(domain)
+    monitorMailgun(domain)
+    monitorNovatus(domain)
 
+
+############# Actions ###########################
+def actionDpxNotificationBatch(domain, parameter):
+    print "***** Triggering notification batch ******** "
+    if not parameter:
+        print "Please pass parameter in JSON formmat {'help':true,'run':'continous','args':'all'}"
+    else:
+        try:
+            help = parameter['help']
+            run = parameter['run']
+            args = parameter['args']
+            print help + run + args
+            DPX = "http://" + domain + ".bidsync.com:9180/dpx/rs/dpxbatch/notification?continous?help=" + help + "&run=" + run + "&args=" + args
+            response = getUrlResponse(DPX)
+            print response.text
+
+        except IOError:
+            printError("Error connecting to dpx app. Need to start the app -- wft dpx start")
+
+
+def runbatch(domain, parameter):
+    print "********* Running batch ********"
+    try:
+        URL = ''
+        if parameter == 'indexwebbids':
+            URL = "http://" + domain + ".bidsync.com:9380/bidsync-business-provider/rs/batch/indexWebExtractBids?roundCount=1"
+        elif parameter == 'indexauctions':
+            URL = "http://" + domain + ".bidsync.com:9380/bidsync-business-provider/rs/batch/indexAuctions?roundCount=1"
+        elif parameter == 'mailgunstatuses':
+            URL = "http://" + domain + ".bidsync.com:9380/bidsync-business-provider/rs/batch/mailgunDeliveryStatuses"
+        else:
+            print "Unable to identify which batch to run. Here are the parameters : {indexwebbids | indexauctions | mailgunstatuses}"
+
+        if URL != '':
+            response = getUrlResponse(URL)
+            print "\t Output : " + response.text
+
+
+    except IOError:
+        printError("Error running the batch")
+
+
+############ Actions ##############################
 
 
 def main():
-    myopts, args = getopt.getopt(sys.argv[1:], "d:s:")
+    myopts, args = getopt.getopt(sys.argv[1:], "d:s:a:p:h:")
 
     domain = 'dev'
+    action = ''
+    status = ''
+    parameter = ''
+    helptopic = ''
     for o, a in myopts:
         if o == '-d':
             domain = a
         elif o == '-s':
             status = a
+        elif o == '-a':
+            action = a
+        elif o == '-h':
+            helptopic = a
+        elif o == '-p':
+            try:
+                parameter = json.loads(a)
+            except ValueError:
+                parameter = a;
+
         else:
             print("Usage: %s -d input -s output" % sys.argv[0])
+
     if status != '':
         if status == 'notification':
             monitorNotification(domain)
@@ -234,12 +321,37 @@ def main():
             monitorElasticSearch(domain)
         elif status == 'tax':
             monitorTax(domain)
+        elif status == 'batches':
+            monitorBatches(domain)
+        elif status == 'muhimbi':
+            monitorMuhimbi(domain)
         else:
-            print "looks like you need some help"
+            print "Type what status you want to see. statuses {all | dpx | ref | notification | cas | jms | smartprocure | elasticsearch | tax | batches }"
+    elif action != '':
+        if action == 'dpxnotificationbatch':
+            actionDpxNotificationBatch(domain, parameter)
+        if action == 'runbatch':
+            runbatch(domain, parameter)
+        else:
+            print "Type what action to perform. actions : {dpxnotificationbatch | runbatch}"
+    elif helptopic != '':
+        helpdir = '/apps/code/aageno/scripts/help/'
+        if helptopic == 'options':
+            files =  glob.glob(helpdir+"*.txt")
+            for file in files:
+                print os.path.splitext(os.path.basename(file))[0]
+        else:
+            subprocess.call(['vi', helpdir+helptopic+'.txt'])
+
 
 
     else:
-        print "Looks like you need some help --"
+        print "Looks like you need some help. -h helptopics"
+
+    from subprocess import Popen, PIPE
+    p1 = Popen(['lsof', '-a', '-p9108', '-i4'], stdout=PIPE)
+    p2 = Popen(["grep", "LISTEN"], stdin=p1.stdout, stdout=PIPE)
+    print p2.communicate()[0]
 
 
 class bcolors:
